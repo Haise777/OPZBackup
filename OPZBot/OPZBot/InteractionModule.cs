bdbe8f562c3de8fa3341158a5a42bccbc1c505d4
Haise_ <gashimabucoro@gmail.com>
2023-12-06 00:16:21 -0300

    finished setting up bot

---
+++ OPZBot/OPZBot/InteractionModule.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Interactions;
+
+namespace OPZBot;
+
+public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("ping", "Receive a ping")]
+    public async Task HandlePingCommand()
+    {
+        await RespondAsync("PING");
+    }
+
+}
--- OPZBot/OPZBot/Program.cs
+++ OPZBot/OPZBot/Program.cs
@@ -1,3 +1,107 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RunMode = Discord.Interactions.RunMode;
 
-Console.WriteLine("Hello, World!");
+namespace OPZBot;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync(args);
+
+    private async Task MainAsync(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("config.json")
+            .Build();
+        
+        using var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((_,services) => services.AddSingleton(config))
+            .ConfigureServices(SetServices)
+            
+            .Build();
+        
+        await RunAsync(host);
+    }
+
+    private void SetServices(HostBuilderContext context, IServiceCollection services)
+    {
+        services
+            .AddSingleton(_ => new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All,
+                AlwaysDownloadUsers = true
+            }))
+            .AddSingleton(provider => new InteractionService(
+                provider.GetRequiredService<DiscordSocketClient>(),
+                new InteractionServiceConfig()
+                {
+                    DefaultRunMode = RunMode.Async
+                }))
+            .AddSingleton<InteractionHandler>()
+            .AddSingleton(_ => new CommandService());
+    }
+    
+    private async Task RunAsync(IHost host)
+    {
+        using IServiceScope serviceScope = host.Services.CreateScope();
+        var provider = serviceScope.ServiceProvider;
+        
+        var client = provider.GetRequiredService<DiscordSocketClient>();
+        var sCommands = provider.GetRequiredService<InteractionService>();
+        await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
+        var config = provider.GetRequiredService<IConfigurationRoot>();
+        
+        
+        client.Log += async msg => Console.WriteLine(msg.Message);
+        sCommands.Log += async msg => Console.WriteLine(msg.Message);
+        
+        
+        client.Ready += async () =>
+        {
+            Console.WriteLine("Bot ready!");
+            await sCommands.RegisterCommandsToGuildAsync(UInt64.Parse(config["testGuild"]));
+        };
+
+        await client.LoginAsync(TokenType.Bot, config["token"]);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
