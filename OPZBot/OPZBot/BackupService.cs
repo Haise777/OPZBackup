f220bd0f10af7b6445a0c930aa9e4a4d60673704
Haise_ <gashimabucoro@gmail.com>
2023-12-08 16:45:55 -0300

    Created Backup Service class

---
+++ OPZBot/OPZBot/BackupService.cs
@@ -0,0 +1,68 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace OPZBot;
+
+public class BackupService
+{
+    private readonly BackupRegister _register;
+    private readonly MessageFetcher _messageFetcher;
+    private SocketInteractionContext _command;
+    private bool _untilLast;
+    private List<User> _users;
+    private AutoMapper _mapper;
+
+    public BackupService(MessageFetcher messageFetcher, AutoMapper mapper)
+    {
+        _messageFetcher = messageFetcher;
+        _mapper = mapper;
+    }
+
+    public async Task Start(SocketInteractionContext command, bool untilLast)
+    {
+        _command = command;
+        _untilLast = untilLast;
+        _users = new List<User>() { _mapper.Map<User>(command.User) };
+        
+        //Build Channel, Author, BackupRegister
+
+        await MakeBackup();
+    }
+
+    private async Task MakeBackup()
+    {
+        ulong lastMessage = 0;
+        bool continueBackup = true;
+        while (continueBackup)
+        {
+            var fetchedMessages = await _messageFetcher.Fetch(_command.Channel, lastMessage);
+            if (!fetchedMessages.Any()) break;
+            var messageBatch = await ProcessMessages(fetchedMessages, out continueBackup);
+            if (!messageBatch.Any()) continue;
+
+            await SaveBatch(messageBatch);
+        }
+    }
+
+    private async Task SaveBatch(IEnumerable<Messages> messageBatch)
+    {
+        
+    }
+
+    private Task<IEnumerable<Messages>> ProcessMessages(IEnumerable<IMessage> messageBatch, out bool continueBackup)
+    {
+        continueBackup = true;
+        var filteredMessages = new List<Message>();
+        foreach (var message in messageBatch)
+        {
+            if (_blacklist.Has(message.Author.Id)) continue;
+            if (_messageRepository.Exists(message))
+            {
+                continueBackup = false;
+                break;
+            }
+            if (!_users.Exists(x => x.Id == message.Author.Id)) 
+            
+        }
+    }
+}
---
+++ OPZBot/OPZBot/MessageFetcher.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace OPZBot;
+
+public class MessageFetcher
+{
+    public async Task<IEnumerable<IMessage>> Fetch(ISocketMessageChannel channel, ulong startFrom)
+    {
+        if (startFrom == 0) return await channel.GetMessagesAsync(10).FlattenAsync();
+
+        return await channel.GetMessagesAsync(startFrom, Direction.Before, 10).FlattenAsync();
+    }
+}
