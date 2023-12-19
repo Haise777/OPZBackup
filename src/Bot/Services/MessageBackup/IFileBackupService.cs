72f6fd33dccf46188bd3c84a0f9c199c630674b9
Haise_ <gashimabucoro@gmail.com>
2023-12-19 05:13:53 -0300

    license headers

--- src/Bot/Services/MessageBackup/IFileBackupService.cs
+++ src/Bot/Services/MessageBackup/IFileBackupService.cs
@@ -1,4 +1,10 @@
-﻿using Discord;
+﻿// Copyright (c) 2023, Gabriel Shimabucoro
+// All rights reserved.
+// 
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+using Discord;
 
 namespace OPZBot.Services.MessageBackup.FileBackup;
 
--- src/Bot/StartupConfigService.cs
+++ src/Bot/StartupConfigService.cs
@@ -1,4 +1,10 @@
-﻿using System.Text.Json;
+﻿// Copyright (c) 2023, Gabriel Shimabucoro
+// All rights reserved.
+// 
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.Configuration;
 
