5c99722e026d196ce51cea735f4245b64881a8e7
gabriel.shimabucoro <gabriel.shimabucoro@grupoprovac.com.br>
2025-02-03 13:00:15 -0300

    progress in implementation of the Embed factory for the Statistics feature

--- opzbackup/OPZBackup/ResponseHandlers/Stats/EmbedResponseFactory.cs
+++ opzbackup/OPZBackup/ResponseHandlers/Stats/EmbedResponseFactory.cs
@@ -1,6 +1,8 @@
-﻿using Discord;
+﻿using System.Text;
+using Discord;
 using OPZBackup.Data.Dto;
 using OPZBackup.Data.Models;
+using OPZBackup.Services.Utils;
 
 namespace OPZBackup.ResponseHandlers.Stats;
 
@@ -37,40 +39,61 @@ public class EmbedResponseFactory
     //     return embedBuilder;
     // }
 
-    internal Embed CreateChannelsStats(IEnumerable<Channel> channels)
+    public Embed CreateChannelsStats(IEnumerable<Channel> channels)
     {
-        var embedBuilder = CreateBaseEmbedBuilder();
-        var channelRows = new List<EmbedFieldBuilder>();
+        var embedBuilder = CreateBaseEmbedBuilder("Status de todos os canais");
+
         int totalMessageCount = 0;
         int totalFileCount = 0;
         ulong compressedByteSize = 0;
 
         foreach (var channel in channels)
         {
-            channelRows.Add(AddChannelStatField(embedBuilder, channel));
             totalMessageCount += channel.MessageCount;
             totalFileCount += channel.FileCount;
             compressedByteSize += channel.CompressedByteSize;
         }
 
         // To add the totals here in a Embed field before the other fields are added
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine($"### Total do servidor em Backup");
+        stringBuilder.AppendLine($"Mensagens totais: {totalMessageCount}");
+        stringBuilder.AppendLine($"Arquivos totais: {totalFileCount}");
+        stringBuilder.AppendLine($"Tamanho total: {compressedByteSize}");
 
-        foreach (var row in channelRows)
-            embedBuilder.AddField(row);
+        embedBuilder.WithDescription(stringBuilder.ToString());
+
+        var channelTable = BuildChannelTableString(channels);
+
+        embedBuilder.AddField("Lista de canais:",
+            $"```\n{channelTable}\n```");
 
         return embedBuilder.Build();
     }
 
-    internal Embed CreateDetailedChannelStats(ChannelStats channelStats)
+    public Embed CreateDetailedChannelStats(ChannelStats channelStats)
     {
+        var embedBuilder = CreateBaseEmbedBuilder("Title");
+
+        // To add the totals here in a Embed field before the other fields are added
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine($"### channel-name");
+        stringBuilder.AppendLine($"Mensagens totais: {channelStats.numberOfMessages}");
+        stringBuilder.AppendLine($"Arquivos totais: {channelStats.numberOfFiles}");
+        stringBuilder.AppendLine($"Tamanho total: {channelStats.bytesize}");
+
+        embedBuilder.WithDescription(stringBuilder.ToString());
 
 
+        var userTable = BuildUserTableString(channelStats.users);
 
+        embedBuilder.AddField("Lista de canais:",
+            $"```\n{userTable}\n```");
 
-        throw new NotImplementedException();
+        return embedBuilder.Build();
     }
 
-    internal Embed CreateUsersStats(IEnumerable<User> users)
+    public Embed CreateUsersStats(IEnumerable<User> users)
     {
         var embedBuilder = CreateBaseEmbedBuilder();
         var userRows = new List<EmbedFieldBuilder>();
@@ -86,7 +109,7 @@ public class EmbedResponseFactory
         return embedBuilder.Build();
     }
 
-    internal Embed CreateDetailedUserStats(UserStats userStats)
+    public Embed CreateDetailedUserStats(UserStats userStats)
     {
 
 
@@ -94,23 +117,72 @@ public class EmbedResponseFactory
         throw new NotImplementedException();
     }
 
-    private EmbedFieldBuilder AddChannelStatField(EmbedBuilder embedBuilder, Channel channel)
+    private string GetChannelStatField(Channel channel)
     {
-        return new EmbedFieldBuilder()
-        .WithName("");
+        return "";
     }
 
-    private EmbedFieldBuilder AddUserStatField(EmbedBuilder embedBuilder, User user)
+    private string BuildChannelTableString(IEnumerable<Channel> channels)
     {
-        return new EmbedFieldBuilder()
-        .WithName("");
+        var builder = new StringBuilder();
+
+        builder.AppendLine("╔════════════════╦═══════════╦══════════╦═════════╗");
+        builder.AppendLine("║ Canal          ║ Mensagens ║ Arquivos ║ Tamanho ║");
+
+        foreach (var channel in channels)
+        {
+            builder.AppendLine("╠════════════════╬═══════════╬══════════╬═════════╣");
+            builder.AppendLine(
+                $"║ {FitText(channel.Name, 14)} ║ {FitText(channel.MessageCount.ToString(), 9)} ║ {FitText(channel.FileCount.ToString(), 8)} ║ {FitText(channel.CompressedByteSize.ToFormattedString(), 8)} ║");
+        }
+
+        builder.Append("╚════════════════╩═══════════╩══════════╩═════════╝");
+        return builder.ToString();
     }
 
-    private EmbedBuilder CreateBaseEmbedBuilder()
+        private string BuildUserTableString(IEnumerable<User> users)
+    {
+        var builder = new StringBuilder();
+
+        builder.AppendLine("╔════════════════╦═══════════╦══════════╦═════════╗");
+        builder.AppendLine("║ Usuário        ║ Mensagens ║ Arquivos ║ Tamanho ║");
+
+        foreach (var user in users)
+        {
+            builder.AppendLine("╠════════════════╬═══════════╬══════════╬═════════╣");
+            builder.AppendLine(
+                $"║ {FitText(user.Username, 14)} ║ {FitText(user.MessageCount.ToString(), 9)} ║ {FitText(user.FileCount.ToString(), 8)} ║ {FitText(user.ByteSize.ToFormattedString(), 8)} ║");
+        }
+
+        builder.Append("╚════════════════╩═══════════╩══════════╩═════════╝");
+        return builder.ToString();
+    }
+
+    string FitText(string text, int width)
+    {
+        // If the text fits within the width, pad it on the right.
+        if (text.Length <= width)
+        {
+            return text.PadRight(width);
+        }
+        else
+        {
+            // If the width is too small to even display ellipsis, just take a substring.
+            if (width <= 3)
+                return text.Substring(0, width);
+            // Otherwise, reserve space for "..." at the end.
+            return text.Substring(0, width - 3) + "...";
+        }
+    }
+
+
+    private EmbedBuilder CreateBaseEmbedBuilder(string title)
     {
         var embedBuilder = new EmbedBuilder();
 
         // Base stuff for the embed here
+        embedBuilder.WithTitle(title);
+        embedBuilder.WithColor(Color.Blue);
 
         return embedBuilder;
     }
