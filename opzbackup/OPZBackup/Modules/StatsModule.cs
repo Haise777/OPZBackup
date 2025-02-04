9600c9ba433407d0a4e9f3c39f11c0197345676f
.haise_san <98202347+Haise777@users.noreply.github.com>
2025-02-04 18:48:05 -0300

    Removed comments of requirements now that they are already implemented.

--- opzbackup/OPZBackup/Modules/StatsModule.cs
+++ opzbackup/OPZBackup/Modules/StatsModule.cs
@@ -7,7 +7,6 @@ namespace OPZBackup.Modules;
 [Group("stats", "utilizar a função de stats")]
 public class StatsModule : InteractionModuleBase<SocketInteractionContext>
 {
-
     private readonly StatsService _statsService;
     private readonly ResponseHandler _responseHandler;
 
@@ -16,14 +15,7 @@ public class StatsModule : InteractionModuleBase<SocketInteractionContext>
         _statsService = statsService;
         _responseHandler = responseHandler;
     }
-
-    //Command structure should be like
-    // Stats -> Channel/User -> ListAll
-    // Stats -> Channel -> InDetails
-    // Stats -> User -> InDetails -> @<userMention>
-
-    // Show a embed with all channels, each containing 
-    // N of messages, N of files, bytesize (for future: active period)
+    
     [SlashCommand("canal-listar", "efetuar backup deste canal")]
     public async Task ListAllChannelStats()
     {
@@ -32,11 +24,7 @@ public class StatsModule : InteractionModuleBase<SocketInteractionContext>
         var channels = await _statsService.ListAllChannelStats();
         await _responseHandler.SendChannelsStatsAsync(channels, Context);
     }
-
-    // Show a embed with all of the above, plus
-    // Each user that sent message at this channel and their
-    // N of messages sent to this channel
-    // N of files sent to this channel
+    
     [SlashCommand("canal-detalhado", "efetuar backup deste canal")]
     public async Task GetInDetailChannelStats()
     {
@@ -45,9 +33,7 @@ public class StatsModule : InteractionModuleBase<SocketInteractionContext>
         var channelStats = await _statsService.GetInDetailChannelStats(Context.Channel.Id);
         await _responseHandler.SendDetailedChannelStatsAsync(channelStats, Context);
     }
-
-    // Show a embed with all users, each containing
-    // N of messages, N of files, bytesize (for future: active period)
+    
     [SlashCommand("usuario-listar", "efetuar backup deste canal")]
     public async Task ListAllUsersStats()
     {
@@ -56,13 +42,7 @@ public class StatsModule : InteractionModuleBase<SocketInteractionContext>
         var users = await _statsService.ListAllUsersStats();
         await _responseHandler.SendUsersStatsAsync(users, Context);
     }
-
-    // Show a embed with all of the above, plus
-    // total num of mention to other users with then number of mention for each individual user
-    // each file type sent with their number of sent
-    // like: image: 20, video: 7, audio: 2, others: 34
-    // top most common words sent inside a message
-    // active period
+    
     [SlashCommand("usuario-detalhado", "efetuar backup deste canal")]
     public async Task GetInDetailUserStats()
     {
