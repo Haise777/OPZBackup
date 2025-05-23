869b675d4335b2c0a6c2a6d394893de7eb370f17
gabriel.shimabucoro <gabriel.shimabucoro@grupoprovac.com.br>
2025-05-23 09:56:58 -0300

    removed temporary code

--- opzbackup/OPZBackup/Services/Stats/MessageStatsProcessor.cs
+++ opzbackup/OPZBackup/Services/Stats/MessageStatsProcessor.cs
@@ -32,7 +32,6 @@ public class MessageStatsProcessor
     {
         var wordCounts = new Dictionary<string, int>();
         var mentionCounts = new Dictionary<ulong, int>();
-        //TODO: Do something about this
 
         foreach (var message in messageList)
         {
@@ -102,33 +101,3 @@ public class MessageStatsProcessor
             _fileTypeStats.Increment(attach.Extension);
     }
 }
-
-public class FileTypeStats
-{
-    public int ImageCount { get; private set; }
-    public int VideoCount { get; private set; }
-    public int AudioCount { get; private set; }
-    public int OtherCount { get; private set; }
-
-    public void Increment(string ext)
-    {
-        switch (GetFileType(ext))
-        {
-            case FileType.Image: ImageCount++; break;
-            case FileType.Video: VideoCount++; break;
-            case FileType.Audio: AudioCount++; break;
-            default: OtherCount++; break;
-        }
-    }
-
-    private static FileType GetFileType(string extension)
-        => extension.ToLowerInvariant() switch
-        {
-            "jpg" or "jpeg" or "png" or "gif" => FileType.Image,
-            "mp4" or "mov" or "avi" => FileType.Video,
-            "mp3" or "wav" => FileType.Audio,
-            _ => FileType.Other,
-        };
-}
-
-public enum FileType { Image, Video, Audio, Other }
